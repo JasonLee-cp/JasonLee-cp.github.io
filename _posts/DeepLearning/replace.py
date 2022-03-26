9c31d64c23659cfc480e9bdf336cff17b1fbdf77
Jason Lee <changjin9792@gmail.com>
2022-03-26 15:51:11 +0900

    [POST] DL - Gradient Descent

---
+++ _posts/DeepLearning/replace.py
@@ -0,0 +1,28 @@
+import re
+import os
+
+list_files = os.listdir()
+print(list_files)
+for fname in list_files:
+    if not fname.endswith('.md') or not fname.startswith('2022-03-26'):
+        continue
+    print(fname)
+    contents = ""
+
+    with open(fname, 'r') as f:
+        
+        while True:
+            line = f.readline()
+            
+            if not line:
+                break
+            x = re.search("\$\$.*\$\$", line)
+            if x:
+                o = re.sub("\$\$", "\[[ ", x.string, 1)
+                o = re.sub("\$\$", " \]]", o, 1)
+                line = o
+                
+            contents += line
+            
+    with open(fname, 'w') as f:
+        f.write(contents)
