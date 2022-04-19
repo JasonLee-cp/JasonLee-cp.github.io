d5e4cb5af307f711ed43a9ef5e37b956948c11e2
Jason Lee <changjin9792@gmail.com>
2022-04-19 12:17:42 +0900

    [LA] Inverse Matrix

---
+++ _posts/Mathematics/linearalgebra/replace.py
@@ -0,0 +1,27 @@
+import re
+import os
+
+list_files = os.listdir()
+print(list_files)
+for fname in list_files:
+    if not fname.endswith('.md') or not fname.startswith('2022-04-19'):
+        continue
+    contents = ""
+
+    with open(fname, 'r') as f:
+        
+        while True:
+            line = f.readline()
+            
+            if not line:
+                break
+            x = re.search("\$\$.*\$\$", line)
+            if x:
+                o = re.sub("\$\$", "\[[ ", x.string, 1)
+                o = re.sub("\$\$", " \]]", o, 1)
+                line = o
+                
+            contents += line
+            
+    with open(fname, 'w') as f:
+        f.write(contents)
